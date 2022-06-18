1cba0093c4e5dac87617af890a6625667114e064
hugowea <z.azizov@innopolis.university>
2022-06-18 13:18:10 +0300

    add all changes in authorization_manager_test.dart

--- reusable_learning_app/test/authorization_manager_test.dart
+++ reusable_learning_app/test/authorization_manager_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -8,51 +10,66 @@ import 'package:reusable_app/models/utilities/token_api.dart';
 import 'package:reusable_app/screens/forms/auth_form.dart';
 
 void main() {
-  test('yes', () async {
-    Future<AccountCreateResult> result = AuthorizationManager()
-        .registerAccount("zufar@mail.ru", "user1234", "helloworld");
+  group('Manager works fully', () {
+    //check whole group, not individual test
+    test('Registration of new account works successfully', () async {
+      Future<AccountCreateResult> result = AuthorizationManager()
+          .registerAccount("test1@mail.ru", "test1", "test1");
+      var res = await result;
+      expect(res.errorMessage, null);
+    });
+    test('Authorization of registered account works successfully', () async {
+      Future<AuthResult> result =
+          AuthorizationManager().authorize("test1", "test1");
+      var res = await result;
+      expect(res.isAuthorized, true);
+    });
+    test('Checking if user is authorized works successfully', () async {
+      Future<bool> result = AuthorizationManager().isAuthorized();
+      expect(await result, true);
+    });
+    AuthorizationManager().logout();
+  });
+  test('registration works successfully', () async {
+    Future<AccountCreateResult> result =
+        AuthorizationManager().registerAccount("test2", "test2", "test2");
     var res = await result;
     expect(res.errorMessage, null);
   });
+  test('user cannot authorize if its name not in database', () async {
+    Future<AuthResult> result =
+        AuthorizationManager().authorize("test3", "test3");
+    var res = await result;
+    expect(res.isAuthorized, false);
+    expect(
+        res.errorMessage, "No active account found with the given credentials");
+  });
+  test(
+      'If user authorizes with null entries, it is probably loss of server connection',
+      () async {
+    Future<AuthResult> result = AuthorizationManager().authorize("", "");
+    var res = await result;
+    expect(res.isAuthorized, false);
+    expect(res.errorMessage, "Connection to server lost.");
+  });
+  group('In case of impossibility to authorize', () {
+    //check whole group, not individual test
+    if (AuthorizationManager().isAuthorized() == true) {
+      AuthorizationManager().logout();
+    }
+    test('Authorization of unknown account fails', () async {
+      Future<AuthResult> result =
+          AuthorizationManager().authorize("test5", "test5");
+      var res = await result;
+      expect(res.isAuthorized, false);
+      expect(res.errorMessage,
+          "No active account found with the given credentials");
+    });
+    test(
+        'Checking if user is authorized works successfully (fails if account is not authorized',
+        () async {
+      Future<bool> result = AuthorizationManager().isAuthorized();
+      expect(await result, false);
+    });
+  });
 }
-// class MockDio extends Mock implements Dio {}
-//
-// void main() {
-//   group('fetchAlbum', () {
-//     TokenApi tokenApi;
-//
-//     test('returns an Album if the http call completes successfully', () async {
-//       final _dio = MockDio();
-//       var data = {"username": "use", "password": "1234"};
-//       // Use Mockito to return a successful response when it calls the
-//       // provided http.Client.
-//       when(_dio.post("${ServerSettings.baseUrl}/users/login/",
-//               data: data,
-//               queryParameters: {"Content-Type": "application/json"}))
-//           .thenAnswer((_) async {
-//         Response? response;
-//         response?.statusCode = 401;
-//         return response!;
-//       });
-//
-//       expect(await AuthorizationManager().authorize("use", "1234"),
-//           AuthResult().isAuthorized);
-//     });
-//
-//     test('throws an exception if the http call completes with an error', () {
-//       final client = MockClient();
-//
-//       // Use Mockito to return an unsuccessful response when it calls the
-//       // provided http.Client.
-//       when(client
-//               .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
-//           .thenAnswer((_) async => http.Response('Not Found', 404));
-//
-//       expect(fetchAlbum(client), throwsException);
-//     });
-//
-//     test('', () {
-//
-//     })
-//   });
-// }
