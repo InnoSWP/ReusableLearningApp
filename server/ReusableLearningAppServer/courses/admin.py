18777ddd169388eb047a9de79c9b4fce5d68f3ce
Yakov Dementyev <y.dementyev@innopolis.university>
2022-06-18 20:28:16 +0300

    Customized admin panels for some models

--- server/ReusableLearningAppServer/courses/admin.py
+++ server/ReusableLearningAppServer/courses/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from courses.models import Course
 
-admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_description', 'display_students', 'display_lessons')
+
+    @admin.display(description='Course description', empty_value='<i>none</i>')
+    def display_description(self, obj):
+        return obj.description[:128]
+
+    @admin.display(description='Number of students', empty_value='0')
+    def display_students(self, obj):
+        return obj.users.count()
+
+    @admin.display(description='Number of lessons', empty_value='0')
+    def display_lessons(self, obj):
+        return obj.lessons.count()
--- server/ReusableLearningAppServer/lessons/admin.py
+++ server/ReusableLearningAppServer/lessons/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from lessons.models import Lesson
 
-admin.site.register(Lesson)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level', 'display_course')
+
+    @admin.display(description='Course name', empty_value='Nothing???')
+    def display_course(self, obj):
+        return obj.course.name
