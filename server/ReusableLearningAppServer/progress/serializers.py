758eb221e6364e9ec3da18cc974f8e6106a5e125
Yakov Dementyev <y.dementyev@innopolis.university>
2022-06-30 09:32:40 +0300

    added API for checking all progress

--- server/ReusableLearningAppServer/progress/serializers.py
+++ server/ReusableLearningAppServer/progress/serializers.py
@@ -1,9 +1,37 @@
 from rest_framework import serializers
 
+from courses.models import Course
+from lessons.models import Lesson
 from progress.models import LessonProgress
 
 
 class LessonProgressSerializer(serializers.ModelSerializer):
     class Meta:
         model = LessonProgress
-        fields = ['user', 'lesson', 'status']
+        fields = ['lesson', 'status']
+
+
+class CourseProgressSerializer(serializers.ModelSerializer):
+    lessons = LessonProgressSerializer(read_only=True, many=True)
+    progress = serializers.IntegerField(read_only=True)
+
+    class Model:
+        model = Course
+        fields = ["id"]
+
+    def to_representation(self, instance):
+        user = self.context["request"].user
+        users_lessons = LessonProgress.objects.filter(lesson__course_id=instance.pk, user=user).all()
+        lessons_amount = Lesson.objects.filter(course=instance).count()
+
+        progress = (lessons_amount and users_lessons.filter(
+            status=LessonProgress.StatusType.COMPLETED
+        ).count()) / (lessons_amount or 1)
+        return {
+            "id": instance.pk,
+            "lessons": LessonProgressSerializer(users_lessons, many=True).data,
+            "progress": progress
+        }
+
+
+
